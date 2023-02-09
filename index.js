95771466375f846471ea035b0ea6d861ec3bbc61
Aleksandr <semen43961@gmail.com>
2023-02-09 15:06:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+"use strict"
+//! 1
+//for(let i = 0; i <= 20; i++) {
+//	if(i %2 !== 0) {
+//		console. log(i);
+//	} else {
+//		continue;
+//	}
+//}
+
+//! 2
+
+//let n = 0;
+
+//while (n < 20) {
+//	n++;
+//	if (n % 2 === 0) {
+//		continue;
+//	}
+//	console.log(n);
+//}
+
+//! 3
+//let age = true;
+
+//let userAge = +prompt("How old are you?");
+
+//while (age) {
+
+//	if (userAge <= 18 || isNaN(userAge)) {
+//		alert("get over here !");
+//		userAge = +prompt("How old are you?");	
+//	}else if (userAge > 18) {
+//		alert("get out of here !");
+//		age = false;
+//	}
+//}
+
+
+//! 4
+for (let i = 0; i <= 10; i++) {
+	for (j = 1; j <= 10; j++)
+	
+}
